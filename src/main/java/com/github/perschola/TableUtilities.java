97b7bad40df21fc836d45f9b7813a7ab41abf4d5
Michal Terranova <michalterranova@mail.com>
2020-11-18 13:19:18 -0500

    Update TableUtilities.java

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -2,14 +2,51 @@ package com.github.perschola;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String table = "";
+        for(int i=1; i<= 5; i++){
+            for(int j=1; j<= 5; j++){
+                if (i*j < 10){
+                    table += "  " + i*j + " |";
+                } else{
+                    table += " " +i*j + " |";
+                }
+            }
+            table += "\n";
+        }
+        return table;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String table = "";
+        for(int i=1; i<= 10; i++){
+            for(int j=1; j<= 10; j++){
+                if (i*j < 10){
+                    table += "  " + i*j + " |";
+                } else if (i*j <100){
+                    table += " " +i*j + " |";
+                } else {
+                    table += i*j + " |";
+                }
+            }
+            table += "\n";
+        }
+        return table;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String table = "";
+        for(int i=1; i<= tableSize; i++){
+            for(int j=1; j<= tableSize; j++){
+                if (i*j < 10){
+                    table += "  " + i*j + " |";
+                } else if (i*j <100){
+                    table += " " +i*j + " |";
+                } else {
+                    table += i*j + " |";
+                }
+            }
+            table += "\n";
+        }
+        return table;
     }
 }
