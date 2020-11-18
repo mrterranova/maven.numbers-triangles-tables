3675b1a9620c60f55bce269646800089ff1015d9
Michal Terranova <michalterranova@mail.com>
2020-11-18 13:18:27 -0500

    Update NumberUtilities.java

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,66 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String numString = "";
+        for(int i = start; i < stop; i++){
+            if(i%2==0){
+                numString += i;
+            }
+        }
+        return numString;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String numString = "";
+        for(int i = start; i < stop; i++){
+            if(i%2 == 1){
+                numString += i;
+            }
+        }
+        return numString;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String numString = "";
+        for(int i = start; i < stop; i+=step){
+            numString += (int)Math.pow(i, 2);
+        }
+        return numString;
     }
 
     public static String getRange(int start) {
-        return null;
+        String numString="";
+        for(int i=0; i < start; i++){
+            numString += i;
+        }
+        return numString;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String numString = "";
+        for(int i=start; i < stop; i++){
+            numString += i;
+        }
+        return numString;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String numString = "";
+        for(int i=start; i < stop; i+=step){
+            numString += i;
+        }
+        return numString;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String numString = "";
+        for (int i=start; i < stop; i+=step){
+            numString += (int)Math.pow(i, exponent)+"";
+        }
+        return numString;
     }
 }
