8400f9f40e14b0daf51334bf0252c56fbc00f590
Michal Terranova <michalterranova@mail.com>
2020-11-18 13:20:05 -0500

    Update TriangleUtilities.java

--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -3,18 +3,46 @@ package com.github.perschola;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String star = "";
+        for (int i = 1; i < numberOfRows; i++) {
+            for (int j = 1; j<= i; j++) {
+                star += "*";
+            }
+            star += "\n";
+        }
+        return star;
+
+
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+//        String stars = "";
+//        for(int i =0; i < numberOfStars; i++){
+//            stars += "*";
+//        }
+//        return stars;
+        return new String(new char[numberOfStars]).replace("\u0000", "*");
     }
 
     public static String getSmallTriangle() {
-        return null;
+        String star = "";
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j<= i; j++) {
+                star += "*";
+            }
+            star += "\n";
+        }
+        return star;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String star = "";
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j<= i; j++) {
+                star += "*";
+            }
+            star += "\n";
+        }
+        return star;
     }
 }
